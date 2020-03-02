34b0f8d7ee0ea899efd9337989806801279c21c0
Ashish Rao <ashishrao2598@gmail.com>
2020-03-02 21:50:35 +0530

    add user domain

--- mvc/domain/user_dao.go
+++ mvc/domain/user_dao.go
@@ -3,6 +3,7 @@ package domain
 import (
 	"fmt"
 	"github.com/ashishra0/go-microservices/mvc/utils"
+	"log"
 	"net/http"
 )
 
@@ -10,9 +11,14 @@ var (
 	users = map[int64]*User {
 		123: {Id: 123, FirstName: "Ashish", LastName: "Rao", Email: "ashish@gmail.com"},
 	}
+
+	UserDao userDao
 )
 
-func GetUser(userId int64) (*User, *utils.ApplicationError) {
+type userDao struct {}
+
+func (u *userDao) GetUser(userId int64) (*User, *utils.ApplicationError) {
+	log.Println("we're accessing the database")
 	if user := users[userId]; user != nil {
 		return user, nil
 	}
