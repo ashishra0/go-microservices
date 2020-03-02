c076f5c973dd715b13a85fed8a2f2f6fe80a31a4
Ashish Rao <ashishrao2598@gmail.com>
2020-03-02 21:51:09 +0530

    add artifacts

--- mvc/services/user_service.go
+++ mvc/services/user_service.go
@@ -5,6 +5,13 @@ import (
 	"github.com/ashishra0/go-microservices/mvc/utils"
 )
 
-func GetUser(userId int64) (*domain.User, *utils.ApplicationError) {
-	return domain.GetUser(userId)
+type usersService struct {
+}
+
+var (
+	UsersService usersService
+)
+
+func (u *usersService) GetUser(userId int64) (*domain.User, *utils.ApplicationError) {
+	return domain.UserDao.GetUser(userId)
 }
